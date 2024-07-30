99881e0be17e2cdf933914077f474e9044f4a202
DevLancelloth <gabriel.silva00@cs.unipe.edu.br>
2024-07-30 15:55:55 -0300

    kekw

--- src/components/WheaterInformations/WeatherInformations.jsx
+++ src/components/WheaterInformations/WeatherInformations.jsx
@@ -10,11 +10,16 @@ function WeatherInformations({ weather }) {
     <div>
       <h2>Meu Componente</h2>
       <p>O nome da cidade é {weather.name}</p>
+      {/* Renderiza a imagem somente se weatherIcon não for nulo */}
       {weatherIcon && (
         <img
           src={`https://openweathermap.org/img/wn/${weatherIcon}.png`}
           alt="Weather icon"
         />
+      )}
+      {/* Verifica se weather.main e weather.main.temp existem antes de acessar */}
+      {weather.main && weather.main.temp !== undefined && (
+        <p>A temperatura é {Math.round(weather.main.temp)}°C</p>
       )}
     </div>
   );
