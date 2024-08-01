92ff453b48fcd8b23d9f926dfc26725846c5e077
DevLancelloth <gabriel.silva00@cs.unipe.edu.br>
2024-08-01 10:41:03 -0300

    subindo estilos de WheaterInformations

--- src/components/WheaterInformations/WeatherInformations.jsx
+++ src/components/WheaterInformations/WeatherInformations.jsx
@@ -1,6 +1,7 @@
 /* eslint-disable no-unused-vars */
 import React from "react";
 import PropTypes from "prop-types";
+import "./WeatherInformations.css";
 
 function WeatherInformations({ weather }) {
   console.log(weather); // Verifica o conteúdo do objeto weather no console
@@ -27,19 +28,28 @@ function WeatherInformations({ weather }) {
     weather.main && weather.main.humidity !== undefined
       ? weather.main.humidity
       : "N/A";
+  const pressure =
+    weather.main && weather.main.pressure !== undefined
+      ? weather.main.pressure
+      : "N/A";
 
   return (
-    <div>
-      <h2>Meu Componente</h2>
-      <p>O nome da cidade é {cityName}</p>
-      <img
-        src={`https://openweathermap.org/img/wn/${weatherIcon}.png`}
-        alt="Weather icon"
-      />
-      <p>A temperatura é {temperature}°C</p>
+    <div className="weather-container">
+      <div>
+        <h2>Meu Componente</h2>
+        <p>O nome da cidade é {cityName}</p>
+        <img
+          src={`https://openweathermap.org/img/wn/${weatherIcon}.png`}
+          alt="Weather icon"
+        />
+        <p className="temperature">A temperatura é {temperature}°C</p>
+      </div>
       <p>Descrição do clima: {weatherDescription}</p>
-      <p>Sensação Térmica: {feelsLike}°C</p>
-      <p>Umidade: {humidity}%</p>
+      <div>
+        <p>Sensação Térmica: {feelsLike}°C</p>
+        <p>Umidade: {humidity}%</p>
+        <p>Pressão Atmosférica: {pressure} hPa</p>
+      </div>
     </div>
   );
 }
